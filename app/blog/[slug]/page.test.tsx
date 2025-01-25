4318366cdb1ee75904714e9f7af164a7192d3e59
Shaydee Coder <shaydeecoder@gmail.com>
2025-01-25 20:49:55 +0100

    fix: Update test for blog/[slug]

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -1,11 +1,15 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Page from "./page";
 
-it("App Router: Works with dynamic route segments", () => {
-  render(<Page params={{ slug: "Test" }} />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
+it("App Router: Works with dynamic route segments", async () => {
+  const params = Promise.resolve({ slug: "Test" });
+  render(<Page params={params} />);
+
+  await waitFor(() => {
+    expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
+  });
 });
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,10 +4,6 @@ type Params = {
   }>;
 };
 
-// export async function generateMetadata({ params }: Params) {
-//   return { title: `Post: ${params.slug}` };
-// }
-
 export default async function Page({ params }: Params) {
   const { slug } = await params;
   return <h1>Slug: {slug}</h1>;
